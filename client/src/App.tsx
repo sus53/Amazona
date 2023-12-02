09ec0f3cc2c27509676eeb71e783c9ddb6598b59
sus53 <manandharsrijan53@gmail.com>
2023-12-02 14:15:03 +0545

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <header>TS amazona</header>
+        <main>yo</main>
+        <footer>All right reserved.</footer>
+      </div>
+  )
+}
+
+export default App
